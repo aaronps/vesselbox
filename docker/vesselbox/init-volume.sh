6aaf749d95fe37de6e7e8afeef54c475bd5a8755
aaronps <aaronperezsanchez@hotmail.com>
2026-02-24 19:45:15 +0800

    Add ubuntu 22.04 Dockerfile and license headers

--- docker/vesselbox/init-volume.sh
+++ docker/vesselbox/init-volume.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+# Copyright 2026 Aaron Perez Sanchez <aaronperezsanchez@hotmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 
 VOLUME_DIR=/data
@@ -51,7 +65,7 @@ VOLUME_HAS_DATA=$(find $VOLUME_DIR -maxdepth 0 ! -empty)
 
 # final tests before running dangerous code
 
-[[ "$VOLUME_HAS_DATA" && ! "$REINIT_OP" ]] && { echo "Volume at $VOLUME_DIR has data, use --reinit to delete data or --overwrite to just extract over it"; }
+[[ "$VOLUME_HAS_DATA" && ! "$REINIT_OP" ]] && { echo "Volume at $VOLUME_DIR has data, use --reinit to delete data or --overwrite to just extract over it"; exit 1; }
 
 
 # execute!
